90131a84fdcacd1db90c35b29d3b9bca104af74e
dvher <diego.vergara5@mail.udp.cl>
2023-11-17 23:00:03 -0300

    Finished get_threats

--- get_threats.py
+++ get_threats.py
@@ -12,9 +12,43 @@ params = dict(
     types='alerts,traffic'
 )
 
+resulting_geojson = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
 resp = requests.get(url=url, params=params)
 
 res = resp.json()
 
-with open('./threats.json', 'w') as f:
-    f.write(json.dumps(res, indent=4))
+alerts = res['alerts']
+
+for i, alert in enumerate(alerts):
+    alert_type = alert['type']
+    alert_subtype = alert['subtype']
+    alert_reliability = alert['reliability']
+    alert_confidence = alert['confidence']
+    alert_location = alert['location']
+
+    x, y = alert_location['x'], alert_location['y']
+
+    resulting_geojson['features'].append({
+        "type": "Feature",
+        "id": f"alert_{i:04d}",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                x,
+                y
+            ]
+        },
+        "properties": {
+            "type": alert_type,
+            "subtype": alert_subtype,
+            "reliability": alert_reliability,
+            "confidence": alert_confidence
+        }
+    })
+
+with open('./threats.geojson', 'w') as f:
+    f.write(json.dumps(resulting_geojson, indent=4))
